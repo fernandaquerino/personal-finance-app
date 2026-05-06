b58bcbe168e0423c784d79a7d3bb30441224c02c
fernandaquerino <fequerinodelima@gmail.com>
2026-05-05 22:24:07 -0300

    chore: resets css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Finance',
-  description: 'Personal finance app',
+  title: 'Personal Finance App',
+  description: 'Personal Finance App',
 };
 
 export default function RootLayout({
