e567a74bc286f866b28299bc889165f806a19630
fernandaquerino <fequerinodelima@gmail.com>
2026-05-27 01:46:15 -0300

    feat: create page for Budgets

---
+++ src/app/(dashboard)/budgets/page.tsx
@@ -0,0 +1,9 @@
+export default function BudgetsPage() {
+  return (
+    <div className="p-400 lg:p-1000">
+      <div className="mb-800 flex items-center justify-between">
+        <h1 className="text-preset-1 text-grey-900">Budgets</h1>
+      </div>
+    </div>
+  );
+}
